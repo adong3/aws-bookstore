e642c28bce799ea94eb5aadf2e842aeb30b7edea
邵煜东 <yudong@shous-MacBook-puro.local>
2020-03-30 19:39:23 +0800

    m

--- toai.php
+++ toai.php
@@ -4,10 +4,32 @@ $con = mysqli_connect('remotemysql.com', 'HDUhlHSVId', 'pCyPrxpWve', 'HDUhlHSVId
 $products = "SELECT * FROM products";
 $grab = $con->query($products);
 
-if (!$con) {
-    echo "fail";
-} else {
-    echo "succeed";
-}
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+
+    <title>welcome to yudongWeb</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+   
+
+    <!-- Custom styles for this template -->
+    <link href="css/keren.css" rel="stylesheet">
+
+    
+</head>
+<body>
+    Yes
+</body>
+</html>
