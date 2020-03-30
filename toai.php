72a4df8abc23c31828e37e525a2a884c43182f4e
邵煜东 <yudong@shous-MacBook-puro.local>
2020-03-30 19:45:05 +0800

    m

--- toai.php
+++ toai.php
@@ -32,6 +32,63 @@ $grab = $con->query($products);
 <body>
     Yes it works fine now.
 
+    <div class="container" >
+    <div class="row">
+        <div class="row">
+            <div class= "col-md-4" >
+                <div class="cart-block" style="background: gold; border: solid 2px;">
+                    <h3 style="padding: 5px"><strong>Welcome to YudongWeb</strong></h3>
+                    <br>
+                    <h5>a personal bookstore</h5>
+                </div>
+                <br>
+                <div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Category</div>
+  
+
+  <!-- List group -->
+  <ul class="list-group">
+    <li class="list-group-item"><a href="vc1.php">HTML CSS Javascript</a></li>
+    <li class="list-group-item"><a href="vc2.php">Machine Learning</a>/li>
+    <li class="list-group-item"><a href="vc3.php">Operating System</a></li>
+    <li class="list-group-item"><a href="vc4.php">Mobile Development</a></li>
+    <li class="list-group-item"><a href="vc5.php">MySQL php related</a></li>
+  </ul>
+</div>
+                <div>
+                    <img src="img/webh.jpg">
+                </div>
+            </div>
+            <div class= "col-md-8" >
+                <div class="panel panel-default">
+                    <!-- Default panel contents -->
+                    <div class="panel-heading"><h3>Books on Sale</h3></div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <?php while ($row = mysqli_fetch_assoc($grab)): ?>
+                                <div class="col-md-4 game">
+                                    <div>Price: $<?php echo $row['price']; ?></div>
+                                    <img src="img/<?php echo $row['image']; ?>" height="160px" width="120px%">
+                                    <div>Title: <?php echo $row['title']; ?></div>
+
+                                </div>
+
+                            <? endwhile;?>
+
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+               
+    </div>
+
+</div><!-- /.container -->
+
 
 
 
